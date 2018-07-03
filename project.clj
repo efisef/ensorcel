148ee122528dbf678b0e9f96608da869fcec7031
tetigi <luke.tomlin@gmail.com>
2018-07-03 11:44:20 +0100

    Updated project defn

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ensorcel "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Easier client / server interactions for Clojure[script]"
+  :url "https://github.com/efisef/ensorcel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
