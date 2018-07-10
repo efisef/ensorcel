a0b03b2b459089c9c0c36b5d7d1b878c03a73d95
tetigi <luke.tomlin@gmail.com>
2018-07-10 12:56:22 +0100

    Tagged new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject efisef/ensorcel "0.1.1-2"
+(defproject efisef/ensorcel "0.1.2"
   :description "Easier client / server interactions for Clojure[script]"
   :url "https://github.com/efisef/ensorcel"
   :license {:name "Eclipse Public License"
