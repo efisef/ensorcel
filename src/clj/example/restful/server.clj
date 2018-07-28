d8cb18a29d44b518492538ed755ff1dcc9c9cebc
Luke Tomlin <luke.tomlin@gmail.com>
2018-07-28 21:00:34 +0100

    Forgot to commit some stuff

--- src/clj/example/restful/server.clj
+++ src/clj/example/restful/server.clj
@@ -1,42 +1,9 @@
 (ns example.restful.server
   (:require [org.httpkit.server :refer [run-server]]
             [ring.middleware.cors :refer [wrap-cors]]
-            [ring.util.http-response :refer [not-found!]]
             [ensorcel.conjure :as c]
-            [ensorcel.conjure.resource.atom :refer [resource]]
+            [ensorcel.resource.atom :refer [resource]]
             [example.restful.api :as api]))
-
-(def defaults
-  {:start-id nil
-   :next-id (fn [_] (str (java.util.UUID/randomUUID)))})
-
-(defn restful-atom
-  [spellbook service & opts]
-  (let [{:keys [start-id next-id]} (merge defaults (apply hash-map opts))
-        service-path (get-in spellbook [service :path])
-        data (atom {:store {} :next-id start-id})]
-    [spellbook
-     service
-     :get (fn [{id :id}] (if-let [result (get-in @data [:store id])]
-                           result
-                           (not-found! {:id id})))
-     :get-all (fn [] (or (vals (@data :store)) []))
-     :new (fn [contents]
-            (let [{store :store id :next-id} @data
-                  item (assoc contents :id id)]
-              (swap! data assoc :next-id (next-id id))
-              (swap! data assoc-in [:store id] item)
-              (format "/api/%s/%s" service-path id)))
-     :edit (fn [{:keys [id] :as item}]
-             (when-not (get-in @data [:store id])
-               (not-found! {:id id}))
-             (swap! data assoc-in [:store id] item)
-             item)
-     :delete (fn [{:keys [id]}]
-               (if-let [item (get-in @data [:store id])]
-                 (do (swap! data update :store dissoc id)
-                     item)
-                 (not-found! {:id id})))]))
 
 (def service
   (apply c/service (resource api/spellbook :postbox
