c39539f611bf721f22b60dda1283bea09d6d689e
tetigi <luke.tomlin@gmail.com>
2018-08-04 13:08:19 +0100

    .

--- src/clj/ensorcel/conjure.clj
+++ src/clj/ensorcel/conjure.clj
@@ -19,7 +19,8 @@
   Raises <raise!> if not."
   [received expected raise!]
   (when (and expected (s/check expected received))
-    (raise! (get received :error (s/check expected received))))
+    (println "!")
+    (raise! (str (get received :error (s/check expected received)))))
   received)
 
 (defn arg-count [f]
